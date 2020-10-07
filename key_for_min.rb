4615006f84f518e3ebcdff9284bc5180e3aa4724
madelinemc <madeline.coons@gmail.com>
2020-10-07 13:27:26 -0500

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,9 @@
+require'pry'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(name_hash) #accepts argument of a hash of numbers {:a => 1, :b => 5, :c => 10}
+    age_array = []
+    age_array = name_hash.collect { |name, age| age }
+    return name_hash.index(age_array.sort[0])
 end
